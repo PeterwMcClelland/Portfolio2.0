d2e635a8a9dd70cd4ad41629bcfa1bfab4070727
Peter McClelland <peterwmcclelland@gmail.com>
2023-06-02 22:27:41 -0500

    hero page

---
+++ src/components/HeroSection/herosection.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function HeroSection() {
+  return (
+    <body>
+    <div className='nav'>
+      <div className='nav-container'>
+        PM
+      </div>
+      </div>
+
+      <div className='main-section'>
+        <div className='columns'>
+          <div className='hero-text'>
+          test
+          </div>
+          <div className='hero-img'>
+            Image
+          </div>
+        </div>
+      </div>
+      
+      </body>
+    
+  
+  )
+}
+
+export default HeroSection;
